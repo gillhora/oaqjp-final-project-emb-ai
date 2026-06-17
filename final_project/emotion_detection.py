58ac55355f63f8ec2addcbcc8171213acf23cfb5
Gilvan Santos da Hora <g.hora@epg-solution.com>
2026-06-17 21:09:58 +0200

    Changed emotion_detection.py to return dictionary

--- final_project/emotion_detection.py
+++ final_project/emotion_detection.py
@@ -11,4 +11,24 @@ def emotion_detector(text_to_analyse): # Define a function named emotion_detecto
     header = { "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock" } # Set the headers required for the API request 
     response = requests.post(url, json = myobj, headers=header) # Send a POST request to the API with the text and headers 
 
-    return response.text
+    # If the response status code is 200, extract the label and score from the response
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+
+        data = [
+                (formatted_response['emotionPredictions'][0]['emotion']['anger'], "anger"),
+                (formatted_response['emotionPredictions'][0]['emotion']['disgust'], "disgust"),
+                (formatted_response['emotionPredictions'][0]['emotion']['fear'], "fear"),
+                (formatted_response['emotionPredictions'][0]['emotion']['joy'], "joy"),
+                (formatted_response['emotionPredictions'][0]['emotion']['sadness'], "sadness")
+        ]
+        dominant_emotion = max(data, key=lambda item: item[0])[1]
+
+    return {
+            'anger': data[0][0],
+            'disgust': data[1][0],
+            'fear': data[2][0],
+            'joy': data[3][0],
+            'sadness': data[4][0],
+            'dominant_emotion': dominant_emotion
+    }
